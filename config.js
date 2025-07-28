7493b4239caa9c451b9ef14542cf441679db0c90
T1T4N119 <pududasuka625@gmail.com>
2025-07-28 14:00:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "wIgCQYyb#YNcuJfMYMR8rJgLC4BcLYLnqT1DkZsZs2hKNSQaxfJc"
-ALIVE_IMG : process.env.ALIVE_IMG || "https://ibb.co/LDh4QwTW",
+module.exports = {
+  ALIVE_IMG: process.env.ALIVE_IMG || "https://ibb.co/LDh4QwTW",
 ALIVE_MSG : process.env.ALIVE_MSG || "*🤖𝐇𝐞𝐲 𝐈'𝐦 💃𝐎𝐛𝐢𝐭𝐨-𝐌𝐃 🤍 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩 𝐁𝐨𝐭⚡*\n\n*🔔𝐈'𝐦 𝐀𝐥𝐢𝐯𝐞 𝐍𝐨𝐰🎠*\n\n*⚖️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - : 𝐎𝐁𝐈𝐓𝐎",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
